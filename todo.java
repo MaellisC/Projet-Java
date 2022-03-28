04ad9534a50f78fa5c50f33799648374537854e2
hafida <hafida.charaf@gmail.com>
2022-03-28 16:26:55 +0200

    premiercommit

--- todo.java
+++ todo.java
@@ -1,7 +1,20 @@
 import javafx.scene.image.Image;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import javafx.application.Application;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.canvas.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.*;
+import javafx.scene.image.*;
 
 public class todo {
 
+    public static void main(String[] args) {
+
+    }
 }
 
 
