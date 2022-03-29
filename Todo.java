92bb4162564324bd0c0686df37d382f7af96a05f
Iorgadmin <Y.chafchaouni@gmail.com>
2022-03-29 11:00:08 +0200

    ajout graph

--- Todo.java
+++ Todo.java
@@ -3,6 +3,8 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -13,7 +15,7 @@ public class Todo extends Application {
         Group root = new Group();
         Scene theScene = new Scene(root);
         theStage.setScene(theScene);
-        Canvas canvas = new Canvas(500, 400);
+        Canvas canvas = new Canvas(423, 669);
         root.getChildren().add(canvas);
         GraphicsContext gc = canvas.getGraphicsContext2D();
         gc.setFill( Color.BLUE );
@@ -23,6 +25,20 @@ public class Todo extends Application {
         gc.setFont( theFont );
         gc.fillText( "Todo list", 40, 50 );
         gc.strokeText( "Todo list", 40, 50 );
+        Image Moistodo = new Image( "Moistodo.png" );
+        gc.drawImage( Moistodo, 0, 0 );
+            Button button_janvier = new Button("Janvier");
+            root.getChildren().add(button_janvier);
+            button_janvier.setTranslateX(64);
+            button_janvier.setTranslateY(120);
+        Button button_fevrier = new Button("Fevrier");
+        root.getChildren().add(button_fevrier);
+        button_fevrier.setTranslateX(189);
+        button_fevrier.setTranslateY(120);
+        Button button_Mars = new Button("Mars");
+        root.getChildren().add(button_Mars);
+        button_Mars.setTranslateX(318);
+        button_Mars.setTranslateY(120);
         theStage.show();
     }
 }
---
+++ page_fetes.java
@@ -0,0 +1,28 @@
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+
+public class page_fetes extends Application {
+    public void start(Stage theStage) {
+        Group root = new Group();
+        Scene theScene = new Scene(root);
+        theStage.setScene(theScene);
+        Canvas canvas = new Canvas(423, 669);
+        root.getChildren().add(canvas);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        Image fetes = new Image( "fetes.png" );
+        gc.drawImage( fetes, 0, 0 );
+        gc.fillText( "Lundi", 71, 125 );
+        gc.strokeText( "Lundi", 71, 125 );
+
+                theStage.show();
+    }
+}
