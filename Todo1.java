577d914363da3bb0b150c36e72162055fd74013d
Iorgadmin <Y.chafchaouni@gmail.com>
2022-03-29 11:28:22 +0200

    ajout graph2

--- Todo1.java
+++ Todo1.java
@@ -3,14 +3,9 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
-public class Todo extends Application {
+public class Todo1 extends Application {
     public void start(Stage theStage) {
         Group root = new Group();
         Scene theScene = new Scene(root);
